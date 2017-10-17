def3862cc99d8f5d8f816c11778dd3801ffce658
Joshua Woodward <joshua.woodward@zoom.us>
2017-10-17 12:10:32 -0700

    update docs

--- menudata.js
+++ menudata.js
@@ -77,7 +77,8 @@ var menudata={children:[
 {text:"p",url:"globals.html#index_p"},
 {text:"r",url:"globals.html#index_r"},
 {text:"s",url:"globals.html#index_s"},
-{text:"u",url:"globals.html#index_u"}]},
+{text:"u",url:"globals.html#index_u"},
+{text:"v",url:"globals.html#index_v"}]},
 {text:"Functions",url:"globals_func.html"},
 {text:"Enumerations",url:"globals_enum.html"},
 {text:"Enumerator",url:"globals_eval.html",children:[
